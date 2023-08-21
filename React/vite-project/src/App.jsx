9f92446318167afb61e96f3cacfc1f95428d4b2e
Kacper <kacpercybulski@icloud.com>
2023-08-21 16:42:17 +0200

    .

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -4,39 +4,15 @@ import Header from './components/Header'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const navbar = (
+	<nav>
+		<h1>Bob's Bistro</h1>
+		<ul>
+			<li>Menu</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
+)
 
-  return (
-    <>
-      <div>
-
-<Header />
-
-
-    
-
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1> jebać zakolaka cwela</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+ReactDOM.render(navbar, document.getElementById('root'))
