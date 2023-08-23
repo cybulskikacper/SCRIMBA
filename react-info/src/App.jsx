f622fea1ffd0e3938b219123186e798f1821f3e7
Kacper <kacpercybulski@icloud.com>
2023-08-23 15:14:17 +0200

    .

---
+++ react-info/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Navbar() {
+	return (
+		<>
+			<nav>
+				<h1>Bob's Bistro</h1>
+				<ul>
+					<li>Menu</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		</>
+	)
+}
+
+export default Navbar
