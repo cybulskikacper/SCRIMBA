fe3d35b99de9b8d8b23eb3615d35d77a8e9f48a9
Kacper <kacpercybulski@icloud.com>
2023-08-27 12:23:00 +0200

    .

--- react-info/src/App.jsx
+++ react-info/src/App.jsx
@@ -28,24 +28,24 @@ function Page() {
 		</>
 	)
 }
-
-function Footer() {
-	return (
-		<footer>
-			<p> © 2023 Cybulski development. All rights reserved.</p>
-		</footer>
-	)
-}
-
 function MainContent() {
 	return (
-		<>
-			<h1>Reasons why I'm excited to learn React</h1>
+		<div className="reasons">
+			<h1 className="header">Reasons why I'm excited to learn React</h1>
 			<ol>
 				<li>I want to learn new skill</li>
 				<li> I want to get hired as a developer</li>
+				<li> To make all video editors jealous that people who know how to code in React can easily make $100 000 per year or even more while all these little video editors are editing some dumb shitty TikTok videos for $7.</li>
 			</ol>
-		</>
+		</div>
+	)
+}
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<p> © 2023 Cybulski development. All rights reserved.</p>
+		</footer>
 	)
 }
 
