9204d843bf63297790422b49fa761f0680f827b7
Kacper <kacpercybulski@icloud.com>
2023-10-24 19:20:45 +0200

    .

---
+++ Meme-generator/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+	return (
+		<>
+			<header className="header">
+				<img className="troll-face" src="./images/troll-face.png" alt="Troll face image" />
+				<h2 className='title'>Meme Generator </h2>
+				<h4 className='text'>React Course - Project 3</h4>
+			</header>
+		</>
+	)
+}
+
+export default Header
