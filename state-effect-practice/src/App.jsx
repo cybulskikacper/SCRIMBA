ee1bbc89ff0a90e16780fe6f70efb07a5b83e645
Kacper <kacpercybulski@icloud.com>
2024-01-04 22:43:13 +0100

    working on a task

--- state-effect-practice/src/App.jsx
+++ state-effect-practice/src/App.jsx
@@ -4,16 +4,18 @@ import WindowTracker from '/WindowTracker.jsx'
 import './App.css'
 
 function App() {
-	/**
-	 * Challenge:
-	 * 1. Create state called `show`, default to `true`
-	 * 2. When the button is clicked, toggle `show`
-	 * 3. Only display `<WindowTracker>` if `show` is `true`
-	 */
+	const [show, setShow] = useState(true)
+
+	const toggleShow = () => {
+		setShow(!show)
+	}
 
 	return (
 		<div className="container">
-			<button>Toggle WindowTracker</button>
+			{/*  Only display `<WindowTracker>` if `show` is `true`
+			 */}
+
+			<button onClick={toggleShow}>Toggle WindowTracker</button>
 			<WindowTracker />
 		</div>
 	)
