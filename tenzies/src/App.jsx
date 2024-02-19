62e7287701499d639b5f5fef9944bc680d1590c8
Kacper <kacpercybulski@icloud.com>
2024-02-19 19:11:20 +0100

    .

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+
+
+import Die from './components/Die'
 
 import './index.css'
 
 function App() {
 	return (
 		<>
-			<div className="square"></div>
+
+			<main>
+
+				<div className="container">
+					<Die value={1} />
+					<Die value={2} />
+					<Die value={3} />
+					<Die value={4} />
+					<Die value={5} />
+					<Die value={6} />
+					<Die value={1} />
+					<Die value={2} />
+					<Die value={3} />
+					<Die value={4} />
+				</div>
+
+			</main>
 		</>
 	)
 }
---
+++ tenzies/src/components/Die.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Die( {value}) {
+
+
+    <div className="die">
+        <span>{value}</span>
+    </div>
+
+
+
+
+
+
+}
