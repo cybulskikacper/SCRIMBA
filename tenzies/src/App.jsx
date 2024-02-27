7b5bf144b0fb3a12c50b8504d9598edb934802b4
Kacper <kacpercybulski@icloud.com>
2024-02-27 23:01:39 +0100

    created a function that returns an array of 10 random numbers

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,28 +1,44 @@
-
-
 import Die from './components/Die'
 
 import './index.css'
 
+/**
+ * Challenge:
+ *
+ * Write a function (allNewDice) that returns an array
+ * of 10 random numbers between 1-6 inclusive.
+ *
+ * Log the array of numbers to the console for now
+ */
+
 function App() {
+	function allNewDice() {
+		const diceArray = []
+
+		for (let i = 0; i< 10; i++) {
+			const randomNumber = Math.floor(Math.random() * 6) + 1
+			diceArray.push(randomNumber)
+		}
+
+		return diceArray
+	}
+	console.log(allNewDice())
+
 	return (
 		<>
-
 			<main>
-
 				<div className="container">
-					<Die value={1} />
-					<Die value={2} />
-					<Die value={3} />
-					<Die value={4} />
-					<Die value={5} />
-					<Die value={6} />
-					<Die value={1} />
-					<Die value={2} />
-					<Die value={3} />
-					<Die value={4} />
+					<Die value="1" />
+					<Die value="2" />
+					<Die value="3" />
+					<Die value="4" />
+					<Die value="5" />
+					<Die value="6" />
+					<Die value="1" />
+					<Die value="1" />
+					<Die value="1" />
+					<Die value="1" />
 				</div>
-
 			</main>
 		</>
 	)
--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 
-export default function Die( {value}) {
-
-
-    <div className="die">
-        <span>{value}</span>
-    </div>
-
-
-
-
-
-
+export default function Die(props) {
+	return (
+		<div className='die-value'>
+			<h2 className='die-num'>{props.value}</h2>
+		</div>
+	)
 }
