880ce09bd6c443db49297b31dcdd929753187045
Kacper <kacpercybulski@icloud.com>
2024-04-06 23:55:25 +0200

    .

--- kurwa-test/src/App.jsx
+++ kurwa-test/src/App.jsx
@@ -4,21 +4,22 @@ export default function App() {
 	const [starWarsData, setStarWarsData] = useState({})
 	const [count, setCount] = useState(1)
 
-	useEffect(() => {
-		const url = `https://swapi.dev/api/people/${count}`
+	console.log(starWarsData)
 
-		fetch(url)
-			.then(res => res.json())
-			.then(data => setStarWarsData(data))
-	}, [count])
+	useEffect(
+		function () {
+			fetch(`https://swapi.dev/api/people/${count}`)
+				.then(res => res.json())
+				.then(data => setStarWarsData(data))
+		},
+		[count]
+	)
 
 	return (
-		<>
-			<div>
-				<h2>The count is {count}</h2>
-				<button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
-				<pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-			</div>
-		</>
+		<div>
+			<h2>The count is {count}</h2>
+			<button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+			<pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+		</div>
 	)
 }
